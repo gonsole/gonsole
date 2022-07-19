0d4138c78bade871cce87525302f824e4b689d43
Uğur Korkmaz <korkmaz.uu@gmail.com>
2022-07-19 18:36:02 +0300

    fix: Script some fixes

--- service/environment/Skeleton.go
+++ service/environment/Skeleton.go
@@ -1,11 +1,11 @@
 package environment
 
 type Skeleton struct {
-	Package   string    `json:"package"`
-	Version   string    `json:"version"`
-	Author    string    `json:"author"`
-	Email     string    `json:"email"`
-	License   string    `json:"license"`
-	Framework Framework `json:"framework"`
-	Script    []Script  `json:"script"`
+	Package   string     `json:"package"`
+	Version   string     `json:"version"`
+	Author    string     `json:"author"`
+	Email     string     `json:"email"`
+	License   string     `json:"license"`
+	Framework Framework  `json:"framework"`
+	Script    []struct{} `json:"script"`
 }
